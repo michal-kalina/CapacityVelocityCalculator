d4163bb2960190e0ad7200c647f00b9d09c4a599
michal-kalina <46845712+michal-kalina@users.noreply.github.com>
2021-07-16 12:08:59 +0200

    Setup initial project with hello world script

--- Build/buildTrigger.sh
+++ Build/buildTrigger.sh
@@ -1,3 +1,27 @@
-#!/bin/bash 
+#!/bin/bash
 
-python -m pip install --user virtualenv
+# get buildTrigger.sh script location
+SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
+cd $SCRIPT_DIR
+echo "Start from root directory"
+cd ..
+pwd
+
+echo "Try to remove venv folder"
+rm -r venv
+
+echo "Install virtual environment"
+# instal virtual environment
+python3 -m pip install --user virtualenv
+# create virtual environment
+python3 -m virtualenv venv
+# enable venv environment
+. venv/bin/activate
+
+#ls -la
+
+echo "Install python modules"
+python3 -m pip install -r requirements.txt
+
+echo "run project"
+python3 Src/main.py
