c531cb1b59baa6bf716a7cf054155227aa305da4
michal-kalina <46845712+michal-kalina@users.noreply.github.com>
2021-07-31 02:39:21 +0200

    Add parametrized fixture

--- src/tests/test_capacity.py
+++ src/tests/test_capacity.py
@@ -1,7 +1,7 @@
 from capacity.capacity import Capacity
 from capacity.people import People
 import re
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
 
@@ -125,6 +125,48 @@ class TestCapacity:
         person2.presents["2021-07-22"].present = False # Tuesday
         person2.presents["2021-07-23"].present = False # Wednesday
         return Capacity([person1, person2])
+
+    @pytest.fixture
+    def get_person_data(self,) -> People:
+        def _get_person_data(date: str, lp: int, days: int) -> People:
+            def _str_to_datetime(date: str) -> datetime:
+                return datetime.strptime(date, "%Y-%m-%d")
+
+            def _generate_days(_date: datetime, _days: int) -> list[str]:
+                return [(_date + timedelta(days=day)).strftime("%Y-%m-%d") for day in range(0,_days)]
+            
+            person = People(f"John{lp}", f"Dou{lp}", _str_to_datetime(date), 14) #14 days
+            for day in _generate_days(datetime.strptime(date, "%Y-%m-%d"), days):
+                person.presents[day].present = False
+
+            return person
+        
+        return _get_person_data
+
+    @pytest.fixture
+    def get_capacity_data(self, get_person_data) -> list[Capacity]:
+        def _get_capacity_data(date: str, data: list[dict[str, int]]) -> list[Capacity]:
+            return Capacity([get_person_data(date, d["lp"], d["days"]) for d in data])
+
+        return _get_capacity_data
+
+    @pytest.mark.parametrize(["test_date", "test_data", "expected_capacity_percentage"], [
+        ("2021-07-19", [{"lp": 1, "days": 2}, {"lp": 2, "days": 3}], 0.75), # people are appsent for 5 days out of 20 which is 0.75 capacity percentage
+        ("2021-07-19", [{"lp": 1, "days": 5}, {"lp": 2, "days": 5}], 0.5),
+        ("2021-07-19", [{"lp": 1, "days": 0}, {"lp": 2, "days": 0}], 1.0),
+        ("2021-07-19", [{"lp": 1, "days": 14}, {"lp": 2, "days": 14}], 0.0),
+    ])
+    def test_people_parametrized(self, get_capacity_data, test_date, test_data, expected_capacity_percentage):
+
+        # Arrange
+        capacity: Capacity = get_capacity_data(test_date, test_data)
+
+        # Act
+        actual_calculated_presence = capacity.calculate_presence()
+
+        # Assert
+        assert actual_calculated_presence == expected_capacity_percentage
+
 
     @pytest.mark.parametrize(["example_data_capacity", "actual_velocity_sp", "expect_velocity_sp"], [
         (pytest.lazy_fixture("example_data_capacity_50"), 50, 25)
