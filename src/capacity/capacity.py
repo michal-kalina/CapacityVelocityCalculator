bc0f44751092d13aea94a42f9e4da88d0d2e3fa1
michal-kalina <46845712+michal-kalina@users.noreply.github.com>
2021-08-04 01:02:36 +0200

    rename file, classes, properties and fix all bugs in test caused by it

--- src/capacity/capacity.py
+++ src/capacity/capacity.py
@@ -1,37 +1,40 @@
+from datetime import datetime
 from re import S
-from .people import Person
+from .person import Person, PresentItem
 from typing import Union
 
 
 class Capacity:
-    def __init__(self, people: Union[list[Person], dict[str, Person]]) -> None:
-        if isinstance(people, dict):
-            self.__people = people
-        elif isinstance(people, list):
-            self.__people = dict[str, Person]()
-            for item in people:
-                self.__people[item.key] = item
+    def __init__(self, persons: Union[list[Person], dict[str, Person]]) -> None:
+        if isinstance(persons, dict):
+            self.__persons = persons
+        elif isinstance(persons, list):
+            self.__persons = dict[str, Person]()
+            for item in persons:
+                self.__persons[item.key] = item
         else:
-            raise TypeError("Unknown type for 'people' arg")
+            raise TypeError("Unknown type for 'persons' arg")
 
-    def get_people(self) -> dict:
-        return self.__people
+    def get_persons(self) -> dict:
+        return self.__persons
 
     def add_person(self, new_person: Person) -> None:
-        self.__people[new_person.key] = new_person
+        self.__persons[new_person.key] = new_person
 
     def set_presence(self, key: str, date: str, value: bool) -> None:
-        self.__people[key].presents[date].present = value
+        self.__persons[key].presences = PresentItem.Create(
+            datetime.strptime(date, "%Y-%m-%d"), value
+        )
 
     def calculate_presence(self):
-        if len(self.__people) == 0:
+        if len(self.__persons) == 0:
             return 0
 
         count = 0
-        for person in self.__people.values():
-            count = count + person.calculate_presence()
+        for persons in self.__persons.values():
+            count = count + persons.calculate_presence()
 
-        return count / len(self.__people)
+        return count / len(self.__persons)
 
     def calculate_actual_capacity(self, velocity_sp: int) -> int:
         presence = self.calculate_presence()
@@ -42,13 +45,9 @@ class Capacity:
         return self
 
     def __next__(self) -> None:
-        if self.__iterIndex == len(self.__people):
+        if self.__iterIndex == len(self.__persons):
             raise StopIteration  # signals "the end"
-        keys = list(self.__people.keys())
-        element = self.__people[keys[self.__iterIndex]]
+        keys = list(self.__persons.keys())
+        element = self.__persons[keys[self.__iterIndex]]
         self.__iterIndex = self.__iterIndex + 1
         return element
-
-
-if __name__ == "__main__":
-    pass
