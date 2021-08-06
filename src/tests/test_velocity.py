78e1cd0588526572684c0b1c0ec427d2cfe3da98
michal-kalina <46845712+michal-kalina@users.noreply.github.com>
2021-08-06 18:23:42 +0200

    Clean code

--- src/tests/test_velocity.py
+++ src/tests/test_velocity.py
@@ -2,11 +2,12 @@ from velocity.velocity import Velocity, VelocityItem
 
 import pytest
 
+
 class TestVelocityCalculator:
     @pytest.fixture
     def get_velocity(self):
         return Velocity()
-    
+
     @pytest.fixture
     def get_velocity_item(self):
         return VelocityItem()
@@ -15,7 +16,7 @@ class TestVelocityCalculator:
         ["context", "instance"],
         [
             [Velocity(), Velocity],
-        ]
+        ],
     )
     def test_create_instance_when_no_params_provided(self, context, instance):
         # Arrange
@@ -24,18 +25,28 @@ class TestVelocityCalculator:
         # Assert
         assert isinstance(context, instance)
 
-
     @pytest.mark.parametrize(
-        ["sprint_name", "committed_sp", "compleated_sp", "capacity", "expected_calculated_velocity"],
         [
-            ("Sprint 1", 100, 50, 1, 50),
-            ("Sprint 2", 101, 51, 1, 51),
-            ("Sprint 3", 10, 5, 0.000000000000000, 0),
-            ("Sprint 3", 10, 5, 1.000000000000000, 5),
-            ("Sprint 3", 10, 5, 0.8, 4),
-        ]
+            "sprint_name",
+            "committed_sp",
+            "compleated_sp",
+            "capacity",
+            "expected_calculated_velocity",
+        ],
+        [
+            ("Sprint 1", 10, 5, 0.000000000000000, 0),  # The smallest amount
+            ("Sprint 2", 10, 5, 1.000000000000000, 5),  # The highest amount
+            ("Sprint 3", 10, 5, 0.5, 2),  # 50%
+        ],
     )
-    def test_create_instance_with_params(self, sprint_name, committed_sp, compleated_sp, capacity, expected_calculated_velocity):
+    def test_create_instance_with_params(
+        self,
+        sprint_name,
+        committed_sp,
+        compleated_sp,
+        capacity,
+        expected_calculated_velocity,
+    ):
         # Arrange
         # Act
         actual = VelocityItem(sprint_name, committed_sp, compleated_sp, capacity)
@@ -45,19 +56,19 @@ class TestVelocityCalculator:
         assert actual.committed_sp == committed_sp
         assert actual.compleated_sp == compleated_sp
         assert actual.capacity == capacity
-        assert actual.calculate_velocity() == expected_calculated_velocity
-
+        assert actual.velocity == expected_calculated_velocity
 
     @pytest.mark.parametrize(
         ["invalid_capacity"],
         [
             [-0.000000000000001],
             [1.000000000000001],
-        ]
+        ],
     )
     def test_raise_error_when_invalid_value_passed(self, invalid_capacity):
         with pytest.raises(
-            ValueError, match=r"Invalid valuse: .*?. The value should be between 0 and 1."
+            ValueError,
+            match=r"Invalid valuse: .*?. The value should be between 0 and 1.",
         ) as exception_info:
             # Arrange
             # Act
--- src/velocity/velocity.py
+++ src/velocity/velocity.py
@@ -1,30 +1,35 @@
 class VelocityItem:
-    def __init__(self, sprint_name, committed_sp, compleated_sp, capacity: float) -> None:
+    def __init__(
+        self, sprint_name, committed_sp, compleated_sp, capacity: float
+    ) -> None:
         self.__sprint_name = sprint_name
         self.__committed_sp = committed_sp
         self.__compleated_sp = compleated_sp
-        if (capacity >= 0 and capacity <= 1):
-            self.__capacity = capacity 
+        if capacity >= 0 and capacity <= 1:
+            self.__capacity = capacity
         else:
-            raise ValueError(f"Invalid valuse: {capacity}. The value should be between 0 and 1.")
-    
+            raise ValueError(
+                f"Invalid valuse: {capacity}. The value should be between 0 and 1."
+            )
+
     @property
     def sprint_name(self) -> str:
         return self.__sprint_name
-    
+
     @property
     def committed_sp(self) -> int:
         return self.__committed_sp
-    
+
     @property
     def compleated_sp(self) -> int:
         return self.__compleated_sp
-    
+
     @property
     def capacity(self) -> int:
         return self.__capacity
 
-    def calculate_velocity(self):
+    @property
+    def velocity(self) -> int:
         return round(self.compleated_sp * self.capacity, 0)
 
 
